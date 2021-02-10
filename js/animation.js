ffa116c34c4d1dffb0bb1ade24a7198e2a5ffda3
sNishant011 <49269819+sNishant011@users.noreply.github.com>
2021-02-10 09:54:23 +0545

    blogs

--- js/animation.js
+++ js/animation.js
@@ -4,16 +4,12 @@ gsap.registerPlugin(ScrollTrigger);
 tl.to(".content li", 1, {
     scrollTrigger:{
         trigger:'.showcase',
-        start:'top top',
-        pin: true,
-        
-
-
+        start:'top center',
     },
-    scale:1.3,
+    scale:1.2,
     y:-100,
     opacity:1,
-    delay:2,
+    delay:0.5,
     stagger: {
       amount: 2,
 
