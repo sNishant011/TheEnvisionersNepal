5f9ba53517814e1a18ee8d2d9e4af7033cbd5d8b
sNishant011 <49269819+sNishant011@users.noreply.github.com>
2021-02-15 11:26:43 +0545

    animations

--- js/animation.js
+++ js/animation.js
@@ -16,3 +16,41 @@ tl.from(".content li", 1, {
 
     }
   });
+var introgsap = gsap.timeline()
+  introgsap.from(".introduction .intro-info p", 1.5, {
+    scrollTrigger:{
+        trigger:'.introduction',
+        start:'top center',
+    },
+    ease: "bounce.out",
+    opacity: 0,
+    x: -100,
+  });
+  introgsap.from(".introduction .intro-info h1", 1, {
+    scrollTrigger:{
+        trigger:'.introduction',
+        start:'top center',
+    },
+    opacity: 0,
+    x: 100,
+  });
+  introgsap.from("#knowmore", 0.5, {
+    scrollTrigger:{
+        trigger:'.introduction',
+        start:'top center',
+    },
+    opacity: 0,
+    y: 50,
+  });
+  
+
+  gsap.to(".introduction .intro-info p", 1, {
+    scrollTrigger:{
+      trigger: '.introduction',
+      start: 'top center',
+      scrub: true,
+    },
+    x:100,
+    y: -50,
+
+  });
